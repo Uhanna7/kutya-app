b897a7e2aaba761ac02771808df28705da24d217
Uri Hanna <h165841@stud.u-szeged.hu>
2023-09-27 09:50:52 +0200

    cím módosítás, pár alap komponens létrehozása

--- kutyakompassz/src/app/app.module.ts
+++ kutyakompassz/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './home/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
